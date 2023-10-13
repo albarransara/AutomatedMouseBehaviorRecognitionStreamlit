0fead0b5ebd3d9606fa28b600548c18a90998ac2
Sara Albarran <saraalbarran@Saras-Mac-mini.home>
2023-10-13 12:14:55 +0200

    fix: Fixed modes

--- frontend_streamlit/frontend.py
+++ frontend_streamlit/frontend.py
@@ -22,7 +22,6 @@ def write_bytesio_to_file(filename,bytesio):
     # Copy the BytesIO stream to the output file
         outfile.write(bytesio.getbuffer())
 
-
 @st.cache_data
 def analyze_files(labels,video_name):
     return annotate_video(labels,video_name,"")
@@ -32,37 +31,116 @@ mode = st.tabs(["Manual","Automatic"])
 zip_name = "results"
 
 # Second page
-with mode[1]:
-
-    st.title("Automatic Mode")
-    st.text("In Automatic Mode you can upload as many videos as you want, as long as\nyou also upload their corresponding DeepLabCut csv files.")
-
-    #model = st.radio("Choose model",("resnet.LSTM","inception_resnet.LSTM","resnet.TCN","inception_resnet.TCN"))
-    st.text("You can choose between the behaviours you want to consider: ")
-    st.checkbox("Grooming", key='grooming_auto')
-    st.checkbox("Mid rearing ", key='mid_rearing_auto')
-    st.checkbox("Wall rearing ", key='wall_rearing_auto')
-
-    st.title("\n")
-
-    uploaded_videos = st.file_uploader("Upload Video files",type=["mp4"],accept_multiple_files=True,key="automatic")
-
-    try:
-        with open(f"{zip_name}.zip","rb") as fp:
-            btn = st.download_button(label="Download results", key='download_auto', data=fp,file_name=f"{zip_name}.zip",mime="application/zip")
-    except IOError:
-            btn = st.button(label="Download results",disabled=True)
-
-# First page
-with (mode[0]):
+with mode[0]:
 
     st.title("Manual Mode")
-    st.text("In Manual Mode you can upload videos with their corresponding DeepLabCut csv file.")
+    st.markdown("In manual mode you can upload videos, with their corresponding DeepLabCut labeled csv, files and you will get the video with labels, as well as some stadistics.")
 
-    st.text("You can choose between the behaviours you want to consider: ")
+    st.markdown("You can choose between the behaviours you want to consider: ")
     grooming_manual = st.checkbox("Grooming", key='grooming_manual')
     mid_rearing_manual = st.checkbox("Mid rearing ", key='mid_rearing_manual')
     wall_rearing_manual = st.checkbox("Wall rearing ", key='wall_rearing_manual')
+
+    uploaded_csvs_manual = st.file_uploader("Upload CSV files",type=["csv"],accept_multiple_files=True,key="manualcsv")
+    uploaded_videos_manual = st.file_uploader("Upload Video files",type=["mp4"],accept_multiple_files=True,key="manualvideos")
+
+    with st.sidebar:
+        time_unit_manual = st.radio("Choose display unit", ("seconds", "frames"),key="manualradio")
+
+    video_names_manual = set()
+
+    for uploaded_video in uploaded_videos_manual:
+        write_bytesio_to_file(uploaded_video.name, uploaded_video)
+        video_names_manual.add(uploaded_video.name[:-4] + ".mp4")
+
+    if len(uploaded_csvs_manual) > 0:
+        tab_names_manual_manual = []
+
+        for ind, uploaded_csv in enumerate(uploaded_csvs_manual):
+            tab_names_manual_manual.append(uploaded_csv.name[:-4])
+
+        does_match_manual = True
+        for csv_name in tab_names_manual_manual:
+            corresponding_video_name = csv_name.split("_")[2] + ".mp4"
+            if corresponding_video_name not in video_names_manual or len(uploaded_csvs_manual) != len(uploaded_videos_manual):
+                st.write("Make sure that each video has a corresponding .csv file and vice-versa")
+                does_match_manual = False
+                break
+
+        if does_match_manual:
+            z_manual = zipfile.ZipFile(f"{zip_name}.zip", mode="w")
+
+            matrix = []
+
+            for uploaded_csv in uploaded_csvs_manual:
+                write_bytesio_to_file(uploaded_csv.name, uploaded_csv)
+                z_manual.write(uploaded_csv.name)
+
+            tabs = st.tabs(tab_names_manual_manual)
+
+            # From the checkboxes, get the behaviours the user wants to predict
+            behaviours_manual = []
+            if grooming_manual:
+                behaviours_manual.append("Grooming")
+            elif mid_rearing_manual:
+                behaviours_manual.append("Rearing mig")
+            elif wall_rearing_manual:
+                behaviours_manual.append("Rearing paret")
+
+            for index, tab in enumerate(tabs):
+                with tab:
+                    df = pd.read_csv(uploaded_csvs_manual[index])
+
+                    labels, distances = analyze_df(df, df[behaviours_manual])
+
+                    video_name = uploaded_csvs_manual[index].name.split("_")[2][:-4] + ".mp4"
+                    fps = analyze_files(labels, video_name)
+
+                    distances['seconds'] = distances['frames'].map(lambda x: x / fps)
+
+                    st.write("fps: ", fps)
+                    z_manual.write("out_" + video_name)
+
+                    video_file = open("out_" + video_name, 'rb')
+                    st.video(video_file)
+
+                    st.write('Horizontal distance traveled over time')
+                    st.line_chart(distances[[time_unit_manual, 'd_x']], x=time_unit_manual)
+
+                    st.write('Vertical distance traveled over time')
+                    st.line_chart(distances[[time_unit_manual, 'd_y']], x=time_unit_manual)
+
+                    st.write('Total distance traveled over time')
+                    st.line_chart(distances[[time_unit_manual, 'd_t']], x=time_unit_manual)
+
+                    st.write('Cumulative horizontal distance traveled over time')
+                    st.line_chart(distances[[time_unit_manual, 'cd_x']], x=time_unit_manual)
+
+                    st.write('Cumulative vertical distance traveled over time')
+                    st.line_chart(distances[[time_unit_manual, 'cd_y']], x=time_unit_manual)
+
+                    st.write('Cumulative total distance traveled over time')
+                    st.line_chart(distances[[time_unit_manual, 'cd_t']], x=time_unit_manual)
+
+            ### CREATE SUMMARY CSV HERE ###
+            distances.to_csv("distance_" + video_name[:-4] + ".csv", index=False)
+            z_manual.write("distance_" + video_name[:-4] + ".csv")
+            z_manual.close()
+
+            with open(f"{zip_name}.zip", "rb") as fp:
+                btn = st.download_button(label="Download results", data=fp, file_name=f"{zip_name}.zip",
+                                         mime="application/zip")
+
+# First page
+with (mode[1]):
+
+    st.title("Automatic Mode")
+    st.markdown("In automatic mode you can upload videos with their corresponding unlabeled DeepLabCut csv files and you will automatically get them labeled.")
+
+    st.markdown("You can choose between the behaviours you want to consider: ")
+    grooming_automatic = st.checkbox("Grooming", key='grooming_automatic')
+    mid_rearing_automatic = st.checkbox("Mid rearing ", key='mid_rearing_automatic')
+    wall_rearing_automatic = st.checkbox("Wall rearing ", key='wall_rearing_automatic')
 
     st.title("\n")
 
@@ -111,11 +189,11 @@ with (mode[0]):
 
             # From the checkboxes, get the behaviours the user wants to predict
             behaviours = []
-            if grooming_manual:
+            if grooming_automatic:
                 behaviours.append("grooming")
-            elif mid_rearing_manual:
+            elif mid_rearing_automatic:
                 behaviours.append("mid_rearing")
-            elif wall_rearing_manual:
+            elif wall_rearing_automatic:
                 behaviours.append("wall_rearing")
 
             # Get results for each video
@@ -131,10 +209,9 @@ with (mode[0]):
                     result_percentage, results = classify_video(df, video_name, "", behaviours)
                     print(results)
 
-                    #TODO
-                    labels, distances = analyze_df(df,results) # TODO arreclar aquest metode,per recuento de distanci i extraccio labels
+                    labels, distances = analyze_df(df,results)
                     
-                    fps = analyze_files(labels,video_name) # TODO arreclar aquest metode,per sobrescriure accions a video
+                    fps = analyze_files(labels,video_name)
 
                     distances['seconds'] = distances['frames'].map(lambda x: x/fps)
 
